39a865fcffbde5d7aae1fae0ffe49348934879d6
raru48486 <raru48486@gmail.com>
2025-05-02 14:21:33 +0900

    テキスト入力部品を実装。

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { Menu, MenuItem, MenuCheckBox, MenuDiv } = rarumenu;
+const { Menu, MenuItem, MenuCheckBox, MenuDiv, MenuInput } = rarumenu;
 
 /** @type {import('./lib/context-menu').MenuOptions} */
 const options = {
     onbeforeopen: (menu) => {
         menu.items.get('div 1').setContent(`${menu.currentTarget.nodeName} ${menu.currentTarget.getAttribute('id')}`);
         menu.items.get('item1').disabled = document.getSelection().isCollapsed;
+        menu.items.get('input 2').disabled = document.getSelection().isCollapsed;
     },
     items: [
         new MenuItem({
@@ -49,6 +50,22 @@ const options = {
             fitContent: true,
             onclick: (menu, item) => {
                 console.log(`${item.id} clicked`);
+            }
+        }),
+        new MenuInput({
+            id: 'input 1',
+            label: '検索',
+            placeholder: '検索条件',
+            onsubmit: (menu, item) => {
+                console.log(`submit ${item.id} with value ${item.value}`);
+            }
+        }),
+        new MenuInput({
+            id: 'input 2',
+            label: '検索',
+            disabled: true,
+            onsubmit: (menu, item) => {
+                console.log(`submit ${item.id} with value ${item.value}`);
             }
         }),
         new MenuDiv({
